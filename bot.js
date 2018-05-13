d6717cb9824e3219fc782fb6e47035755dc07494
Thilina Dissanayake <thilinasupunsara@gmail.com>
2018-05-13 13:50:19 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,4 +1,4 @@
-console.log('the bot is starting.');
+console.log('The bot is starting.');
 
 var Twit = require('twit');
 
@@ -7,7 +7,7 @@ var config = require('./config');
 var Bot = new Twit(config);
 
 
-T.post('statuses/update', { status: 'twitterBot #twitterBot' }, function(err, data, response) {
+Bot.post('statuses/update', { status: 'twitterBot #twitterBot' }, function(err, data, response) {
     console.log(data)
   })
 
