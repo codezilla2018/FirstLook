9121af1f49fa1a3871f34c1d530cf9785a7cd182
unknown <thilinasupunsara@gmail.com>
2018-05-20 20:15:24 +0530

    Updated

--- bot.js
+++ bot.js
@@ -1,16 +1,42 @@
 console.log('the bot is starting.');
 
 var Twit = require('twit');
+var feed = require('feed-read-parser');
 
 var config = require('./config');
 
 var Bot = new Twit(config);
 
+getFeed();
 
-T.post('statuses/update', { status: 'twitterBot #twitterBot' }, function(err, data, response) {
-    console.log(data)
-  })
+function getFeed() {
+    feed("https://www.youtube.com/feeds/videos.xml?channel_id=UCT0hbLDa-unWsnZ6Rjzkfug", function(err,articles) {
+        if(err) {
+            throw new Error(err.message);
+        } else {   
+			/*
+			while 
+			tweet = "" + articles[0].title + ' \n #FirstLook #NewTrailer #NewMovie ' + articles[0].link;
+			//setInterval( function() { Tweet(tweet); }, 60*1000 );
+			*/
+			
+            tweet = "" + articles[0].title + ' \n #FirstLook #NewTrailer #NewMovie ' + articles[0].link;
+			Tweet(tweet);	
+			setInterval( getFeed, 60*1000 );	//6*60*60*1000	= 6 HOURS
+        }
+    });
+}
+
+function Tweet(tweet) {
+    Bot.post('statuses/update', {status: tweet }, function(err, data,response) {
+        if (err) {
+			if(err.code!=187){
+				console.log(err);
+			}           
+        } else {
+            console.log('Bot Tweeted');
+        }
+    });
+}
 
 
-
- 
--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 module.exports = {
-    consumer_key:         '',
-    consumer_secret:      '',
-    access_token:         '',
-    access_token_secret:  ''
+    consumer_key:         'HX6HmA89FU44d7E3bxo2wXLqc',
+    consumer_secret:      'eEwT7Z9AuE5VFF2naKE2yBlLnWWnjKMelK8tJh899lb8k28UYg',
+    access_token:         '994778894120169473-FzLvyL8Q7kC4Vi740s8Tjot4jjoV0yX',
+    access_token_secret:  'nzMvhjKZjuOzvhzlXiIGasOmEFccJFWBwE2fd2kdeqOTx'
 }
